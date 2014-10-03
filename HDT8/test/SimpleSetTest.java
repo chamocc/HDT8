8d6e1fe62288d24afcc0d4515bf62df61dbd242c
chamocc <chamocc@gmail.com>
2014-10-03 07:32:27 -0600

    agregue archivos al source

---
+++ HDT8/test/SimpleSetTest.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Ed. Chamo
+ */
+public class SimpleSetTest {
+    
+    public SimpleSetTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of get method, of class SimpleSet.
+     */
+    @Test
+    public void testGet() {
+        System.out.println("get");
+        Word word = null;
+        SimpleSet instance = new SimpleSet();
+        Word expResult = null;
+        Word result = instance.get(word);
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of add method, of class SimpleSet.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        Word wordObject = null;
+        SimpleSet instance = new SimpleSet();
+        instance.add(wordObject);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}

54f48c261bfd45c3e9577611549a1fd40d3d5303
chamocc <chamocc@gmail.com>
2014-10-08 16:53:10 -0600

    YA SIRVE EL RBT :')

--- carpeta para prueba de programa/RedBlackNode.java
+++ carpeta para prueba de programa/RedBlackNode.java
@@ -1,26 +1,39 @@
-// TOMADO DE http://users.cis.fiu.edu/~weiss/dsaajava/code/DataStructures/
-   
-  
+/**
+ */ // class RedBlackNode
+class RedBlackNode<T extends Comparable<T>> {
 
-    class RedBlackNode
-    {
-            // Constructors
-        RedBlackNode( Comparable theElement )
-        {
-            this( theElement, null, null );
-        }
+    /** Possible color for this node */
+    public static final int BLACK = 0;
+    /** Possible color for this node */
+    public static final int RED = 1;
+	// the key of each node
+	public T key;
 
-        RedBlackNode( Comparable theElement, RedBlackNode lt, RedBlackNode rt )
-        {
-            element  = theElement;
-            left     = lt;
-            right    = rt;
-            color    = RedBlackTree1.BLACK;
-        }
+    /** Parent of node */
+    RedBlackNode<T> parent;
+    /** Left child */
+    RedBlackNode<T> left;
+    /** Right child */
+    RedBlackNode<T> right;
+    // the number of elements to the left of each node
+    public int numLeft = 0;
+    // the number of elements to the right of each node
+    public int numRight = 0;
+    // the color of a node
+    public int color;
 
-            // Friendly data; accessible by other package routines
-        Comparable   element;    // The data in the node
-        RedBlackNode left;       // Left child
-        RedBlackNode right;      // Right child
-        int          color;      // Color
+    RedBlackNode(){
+        color = BLACK;
+        numLeft = 0;
+        numRight = 0;
+        parent = null;
+        left = null;
+        right = null;
     }
+
+	// Constructor which sets key to the argument.
+	RedBlackNode(T key){
+        this();
+        this.key = key;
+	}
+}// end class RedBlackNode
